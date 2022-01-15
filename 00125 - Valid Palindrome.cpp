33f32a066eede728e390abaf6641c8e21731c645
qiweiren <ly.husky@gmail.com>
2022-01-15 03:51:51 -0800

    00125 - Valid Palindrome

---
+++ 00125 - Valid Palindrome.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int i = 0, j = s.size() - 1;
+        while (i < j) {
+            if (!isAlphanumeric(s[i])) {
+                i++;
+            } else if (!isAlphanumeric(s[j])) {
+                j--;
+            } else if (tolower(s[i]) != tolower(s[j])) {
+                return false;
+            } else {
+                i++;
+                j--;
+            }
+        }
+        return true;
+    }
+    
+private:
+    bool isAlphanumeric(char c) {
+        return (c >= 'a' && c <= 'z') ||
+               (c >= 'A' && c <= 'Z') ||
+               (c >= '0' && c <= '9');
+    }
+};
