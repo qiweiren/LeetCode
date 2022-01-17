e9c26e212297ee8fa5350af0e171d1def6d30b21
qiweiren <ly.husky@gmail.com>
2022-01-16 23:53:40 -0800

    00144 - Binary Tree preorder Traversal

--- 00144 - Binary Tree Preorder Traversal.cpp
+++ 00144 - Binary Tree Preorder Traversal.cpp
@@ -1,34 +1,36 @@
 class Solution {
 public:
-    vector<int> preorderTra
+    vector<int> preorderTraversal(TreeNode* root) {
         vector<int> res;
         helper(root, res);
         return res;
     }
     
 private:
-    void helper(TreeNode* r
-        if (root == NULL) r
-        res.push_back(root-
-        helper(root->left, 
-        helper(root->right,
+    void helper(TreeNode* root, vector<int>& res) {
+        if (root == NULL) return;
+        res.push_back(root->val);
+        helper(root->left, res);
+        helper(root->right, res);
     }
 };
+
+
 class Solution {
 public:
-    vector<int> preorderTra
+    vector<int> preorderTraversal(TreeNode* root) {
         vector<int> res;
-        if (root == NULL) r
+        if (root == NULL) return res;
         
         stack<TreeNode*> s;
         s.push(root);
         
-        while (!s.empty()) 
-            TreeNode* cur =
+        while (!s.empty()) {
+            TreeNode* cur = s.top();
             s.pop();
-            res.push_back(c
-            if (cur->right 
-            if (cur->left !
+            res.push_back(cur->val);
+            if (cur->right != NULL) s.push(cur->right);
+            if (cur->left != NULL) s.push(cur->left);
         }
         
         return res;
